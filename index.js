5165e074ad229fb43e78bc67105aff6f45f4318a
mulwajoseph <mulwajose.jm@gmail.com>
2023-08-08 05:27:57 +0300

    lab 2 changes

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
-// function addingEventListener() {
-//     const button = document.getElementById('button')
+//using named function
+function addingEventListener() {
+    const button = document.getElementById('button')
 
-//     function clickAlert(){
-//         alert('I was clicked!')
+    function clickAlert(){
+        alert('I was clicked!')
     
-//     }
-//     button.addEventListener('click',clickAlert)
-// }
-
-
-// addingEventListener()
-//using arrow functiom
+    }
+    button.addEventListener('click',clickAlert)
+}
+addingEventListener()
+//using arrow function/anonymous arrow function
 document.querySelector('#button').addEventListener
 ('click',function(){
     return alert("I was Clicked")
 })
-//to invoke inside a function
-// document.querySelector('#button').addEventListener
-// ('click',clickMe)
-// function clickMe(){
-//     return alert("I was Clicked")
-// }
+//to invoke inside a function/a function directly
+document.querySelector('#button').addEventListener
+('click',clickMe)
+function clickMe(){
+    return alert("I was Clicked")
+}
