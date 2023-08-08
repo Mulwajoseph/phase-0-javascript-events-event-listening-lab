bce62435f1f7e90667446a5a70201dd24417644d
mulwajoseph <mulwajose.jm@gmail.com>
2023-08-08 05:15:38 +0300

    lab 2 changes

--- index.js
+++ index.js
@@ -10,3 +10,14 @@ function addingEventListener() {
 
 
 addingEventListener()
+//using arrow functiom
+document.querySelector('#button').addEventListener
+(click,function(){
+    return alert("I was Clicked")
+})
+//to invoke inside a function
+document.querySelector('#button').addEventListener
+(click,clickMe)
+function clickMe(){
+    return alert("I was Clicked")
+}
